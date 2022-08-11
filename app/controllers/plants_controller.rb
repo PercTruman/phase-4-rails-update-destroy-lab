8a7cd86a2bd40d48de6191a08598cfbc01321c0f
PercTruman <drshawnhart@gmail.com>
2022-08-11 15:35:53 -0500

    commit tree 2f8ce56e561aa1893005c34b32ef53d482a232df

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,26 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(is_in_stock: false)
+      render json: plant
+    else 
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+  end
+
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
 end
